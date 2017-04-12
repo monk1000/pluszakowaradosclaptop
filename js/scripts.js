575c3fc83799fdca699ba1712ef5668782c3226c
Tom_Monk <tomasz.kacprzak1982@gmail.com>
2017-04-12 11:05:43 +0200

    next

--- js/scripts.js
+++ js/scripts.js
@@ -21,51 +21,51 @@ $(function() {
     
 });
 
-
+//
 // contact Form SUBMIT
-$(function(){
-    //zmienne formularz i wiadomość
-    var form = $('#ajax-form');
-    var formMessages = $('#form-messages');
+// $(function(){
+//     //zmienne formularz i wiadomość
+//     var form = $('#ajax-form');
+//     var formMessages = $('#form-messages');
 
 
-    $(form).submit(function(event) {
-        event.preventDefault();
-    });
+//     $(form).submit(function(event) {
+//         event.preventDefault();
+//     });
 
-    var formData = $(form).serialize();
+//     var formData = $(form).serialize();
 
-    $.ajax({
-        type: 'POST',
-        url: $(form).attr('action'),
-        data: formData
+//     $.ajax({
+//         type: 'POST',
+//         url: $(form).attr('action'),
+//         data: formData
 
-    })
-        .done(function(response) {
-            // Make sure that the formMessages div has the 'success' class.
-            $(formMessages).removeClass('error');
-            $(formMessages).addClass('success');
+//     })
+//         .done(function(response) {
+//             // Make sure that the formMessages div has the 'success' class.
+//             $(formMessages).removeClass('error');
+//             $(formMessages).addClass('success');
 
-            // Set the message text.
-            $(formMessages).text(response);
+//             // Set the message text.
+//             $(formMessages).text(response);
 
-            // Clear the form.
-            $('#name').val('');
-            $('#email').val('');
-            $('#message').val('');
-        })
+//             // Clear the form.
+//             $('#name').val('');
+//             $('#email').val('');
+//             $('#message').val('');
+//         })
 
-        .fail(function(data) {
-            // Make sure that the formMessages div has the 'error' class.
-            $(formMessages).removeClass('success');
-            $(formMessages).addClass('error');
+//         .fail(function(data) {
+//             // Make sure that the formMessages div has the 'error' class.
+//             $(formMessages).removeClass('success');
+//             $(formMessages).addClass('error');
 
-            // Set the message text.
-            if (data.responseText !== '') {
-                $(formMessages).text(data.responseText);
-            } else {
-                $(formMessages).text('Oops! An error occured and your message could not be sent.');
-            }
-        })
-});
+//             // Set the message text.
+//             if (data.responseText !== '') {
+//                 $(formMessages).text(data.responseText);
+//             } else {
+//                 $(formMessages).text('Oops! An error occured and your message could not be sent.');
+//             }
+//         })
+// });
 
